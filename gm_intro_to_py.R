53a1a2fc1e2c272016492e76d45082c366de5eae
gmillard756 <gmillard@syr.edu>
2020-10-10 21:11:27 -0400

    learning python

---
+++ gm_intro_to_py.R
@@ -0,0 +1,8 @@
+install.packages("reticulate")
+library(reticulate)
+py_discover_config()
+install.packages("reticulate")
+use_python('C:/Users/gmill/AppData/Local/r-miniconda/envs/r-reticulate/python.exe')
+py_config()
+
+
